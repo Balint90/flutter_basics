6453e0e7ff9d2215e15166e5a3ec7e3bd8b63670
Balint Csertan <csertanb@gmail.com>
2023-08-30 20:28:00 +0200

    Add BottomTabs

--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -1,17 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const BottomTabs());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomTabs(),
+    );
+  }
 }
 
-class BottomTabs extends StatelessWidget {
+class BottomTabs extends StatefulWidget {
   const BottomTabs({super.key});
+
+  @override
+  State<BottomTabs> createState() => _BottomTabsState();
+}
+
+class _BottomTabsState extends State<BottomTabs> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+    Container(color: Colors.green),
+    Container(color: Colors.purple),
+    Container(color: Colors.yellow),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [],
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(_selectedIndex == 0
+                ? 'assets/vectors/home-bold.svg'
+                : 'assets/vectors/home-outline.svg'),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(_selectedIndex == 0
+                ? 'assets/vectors/search-bold.svg'
+                : 'assets/vectors/search-outline.svg'),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(_selectedIndex == 0
+                ? 'assets/vectors/add-square-bold.svg'
+                : 'assets/vectors/add-square.svg'),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(_selectedIndex == 0
+                ? 'assets/vectors/video-play-bold.svg'
+                : 'assets/vectors/video-play-outline.svg'),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 30,
+              height: 30,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/profile.svg'),
+                ),
+                shape: BoxShape.circle,
+              ),
+            ),
+            label: '',
+          ),
+        ],
       ),
     );
   }
