baed348417c4873651c115f42ad1065a3593fc57
Balint Csertan <csertanb@gmail.com>
2023-09-02 20:14:56 +0200

    add timepicker in datepicker project

--- date_picker/lib/main.dart
+++ date_picker/lib/main.dart
@@ -27,35 +27,84 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final TextEditingController _dateController = TextEditingController();
 
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: TextField(
-            controller: _dateController,
-            decoration: const InputDecoration(
-              labelText: 'DATE',
-              filled: true,
-              prefixIcon: Icon(Icons.calendar_today),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide.none,
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: TextField(
+                controller: _dateController,
+                decoration: const InputDecoration(
+                  labelText: 'DATE',
+                  filled: true,
+                  prefixIcon: Icon(Icons.calendar_today),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
                 ),
+                readOnly: true,
+                onTap: () {
+                  _selectDate();
+                },
               ),
             ),
-            readOnly: true,
-            onTap: () {
-              _selectDate();
-            },
-          ),
+            const SizedBox(height: 30),
+            Text(
+              '${_timeOfDay.hour.toString().padLeft(2, '0')}:${_timeOfDay.minute.toString().padLeft(2, '0')}',
+              style: const TextStyle(fontSize: 25),
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              height: 50,
+              minWidth: 200,
+              color: const Color(0xff1D1E22),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              onPressed: () {
+                _selectTime();
+              },
+              child: const Text(
+                'Open Time Picker',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future<void> _selectTime() async {
+    TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _timeOfDay,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null) {
+      setState(() {
+        _timeOfDay = picked;
+      });
+    }
   }
 
   Future<void> _selectDate() async {
